64a3d4a0d0b90b3fc8716da9867b73647aa82efe
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2023-07-24 22:41:08 +0700

    Update ESP32cam_setup V2

--- ESP32cam_setup/ESP32cam_setup.ino
+++ ESP32cam_setup/ESP32cam_setup.ino
@@ -1,156 +1,23 @@
-#include "esp_camera.h"
-#include <WiFi.h>
-#include <Arduino.h>
-#include <EEPROM.h>
-//
-// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
-//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
-//            Partial images will be transmitted if image exceeds buffer size
-//
-//            You must select partition scheme from the board menu that has at least 3MB APP space.
-//            Face Recognition is DISABLED for ESP32 and ESP32-S2, because it takes up from 15 
-//            seconds to process single frame. Face Detection is ENABLED if PSRAM is enabled as well
-
-// ===================
-// Select camera model
-// ===================
-//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
-//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
-//#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
-//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
-//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
-#define CAMERA_MODEL_AI_THINKER // Has PSRAM
-//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
-//#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
-// ** Espressif Internal Boards **
-//#define CAMERA_MODEL_ESP32_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S3_CAM_LCD
-
-#include "camera_pins.h"
-
-// ===========================
-// Enter your WiFi credentials
-// ===========================
+#include "ESP32cam_setup.h"
 
 void startCameraServer();
 void setupLedFlash(int pin);
-
-#include <EEPROM.h>
-
-void saveWifiCredentials(const char* ssid, const char* password) {
-  int ssidLength = strlen(ssid);
-  int passLength = strlen(password);
-
-  // Ghi độ dài của SSID và mật khẩu vào EEPROM (2 byte)
-  EEPROM.write(0, ssidLength);
-  EEPROM.write(1, passLength);
-
-  // Ghi dữ liệu SSID và mật khẩu vào EEPROM (từ byte 2)
-  for (int i = 0; i < ssidLength; i++) {
-    EEPROM.write(i + 2, ssid[i]);
-  }
-  for (int i = 0; i < passLength; i++) {
-    EEPROM.write(i + 2 + ssidLength, password[i]);
-  }
-
-  EEPROM.commit();
-}
-
-void setupSmartConfig() {
-  // Kiểm tra xem đã lưu trữ thông tin Wi-Fi trước đó chưa
-  int ssidLength = EEPROM.read(0);
-  int passLength = EEPROM.read(1);
-
-  if (ssidLength > 0 && passLength > 0) {
-    char* ssid = new char[ssidLength + 1];
-    char* password = new char[passLength + 1];
-
-    // Đọc thông tin SSID và mật khẩu từ EEPROM
-    for (int i = 0; i < ssidLength; i++) {
-      ssid[i] = char(EEPROM.read(i + 2));
-    }
-    ssid[ssidLength] = '\0';
-
-    for (int i = 0; i < passLength; i++) {
-      password[i] = char(EEPROM.read(i + 2 + ssidLength));
-    }
-    password[passLength] = '\0';
-
-    // Kết nối Wi-Fi với thông tin đã lưu trữ
-    WiFi.begin(ssid, password);
-    delete[] ssid;
-    delete[] password;
-    
-    // Chờ kết nối Wi-Fi
-    int timeout = 10; // Thời gian chờ kết nối (giây)
-    while (WiFi.status() != WL_CONNECTED && timeout > 0) {
-      delay(1000);
-      timeout--;
-    }
-
-    if (WiFi.status() == WL_CONNECTED) {
-      // Kết nối Wi-Fi thành công
-      Serial.println("WiFi connected");
-      startCameraServer();
-
-      Serial.print("Camera Ready! Use 'http://");
-      Serial.print(WiFi.localIP());
-      Serial.println("' to connect");
-      
-      return;
-    }
-  }
-
-  // Nếu chưa lưu trữ thông tin Wi-Fi hoặc kết nối không thành công,
-  // tiến hành SmartConfig
-  Serial.println("There is no saved wifi ID and password!");
-  WiFi.beginSmartConfig();
-
-  Serial.println("");
-  Serial.println("Waiting for SmartConfig.");
-  while (!WiFi.smartConfigDone()) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("SmartConfig done.");
-
-  // Lưu trữ thông tin Wi-Fi
-  const char* ssid = WiFi.SSID().c_str();
-  const char* password = WiFi.psk().c_str();
-  saveWifiCredentials(ssid, password);
-
-  // Chờ kết nối Wi-Fi
-  int timeout = 10; // Thời gian chờ kết nối (giây)
-  while (WiFi.status() != WL_CONNECTED && timeout > 0) {
-    delay(1000);
-    timeout--;
-  }
-
-  if (WiFi.status() == WL_CONNECTED) {
-    // Kết nối Wi-Fi thành công
-    Serial.println("WiFi connected");
-    startCameraServer();
-
-    Serial.print("Camera Ready! Use 'http://");
-    Serial.print(WiFi.localIP());
-    Serial.println("' to connect");
-  } else {
-    // Kết nối Wi-Fi không thành công
-    Serial.println("Failed to connect to WiFi");
-  }
-}
+void flashing_led(int num, int freq);
 
 void setup() {
+  //Setup flash
+  pinMode(LED_GPIO_NUM, OUTPUT);
+  flashing_led(5,50);
+
+  //Setup serial and EEPROM
   Serial.begin(115200);
   EEPROM.begin(512);
-  WiFi.mode(WIFI_AP_STA);
   Serial.setDebugOutput(true);
 
+  //Setup wifi mode
+  WiFi.mode(WIFI_AP_STA);
+  
+  //Setup camera
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -178,7 +45,6 @@ void setup() {
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 12;
   config.fb_count = 1;
-  
   // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
   //                      for larger pre-allocated frame buffer.
   if(config.pixel_format == PIXFORMAT_JPEG){
@@ -198,19 +64,12 @@ void setup() {
     config.fb_count = 2;
 #endif
   }
-
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
-
   // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
-
   sensor_t * s = esp_camera_sensor_get();
   // initial sensors are flipped vertically and colors are a bit saturated
   if (s->id.PID == OV3660_PID) {
@@ -222,23 +81,14 @@ void setup() {
   if(config.pixel_format == PIXFORMAT_JPEG){
     s->set_framesize(s, FRAMESIZE_QVGA);
   }
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
-
-#if defined(CAMERA_MODEL_ESP32S3_EYE)
-  s->set_vflip(s, 1);
-#endif
-
 // Setup LED FLash if LED pin is defined in camera_pins.h
 #if defined(LED_GPIO_NUM)
   setupLedFlash(LED_GPIO_NUM);
 #endif
 
-  // Setup Smart Config and server
-  setupSmartConfig();
+  //Setup Smart Config and server
+  setupWifi();
+  flashing_led(5,50);
 }
 
 void loop() {
