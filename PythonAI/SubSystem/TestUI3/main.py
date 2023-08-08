d273dab759ff388871ced1ce983b61a01644329b
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2023-08-09 03:57:42 +0700

    Update UI

--- PythonAI/SubSystem/TestUI3/main.py
+++ PythonAI/SubSystem/TestUI3/main.py
@@ -1,6 +1,18 @@
-
 from os.path import dirname, join
+from PIL import ImageGrab
+from kivy import Config
 
 from app import MainApp
+from kivy.core.window import Window
 
+# Lấy kích thước màn hình
+resolution = ImageGrab.grab().size
+
+# Đặt cửa sổ ứng dụng nằm ở giữa màn hình
+Window.top = (resolution[1] - 720) // 2
+Window.left = (resolution[0] - 1280) // 2
+
+# Đặt kích thước của cửa sổ ứng dụng
+Window.size = (1280, 720)
+# Tạo và chạy ứng dụng chính
 MainApp().run()
